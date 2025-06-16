e6a6b6e4c647e1bac8c0a06190a694382007f5a8
ussaliougueye <sn.saliougueye@gmail.com>
2025-06-15 20:23:40 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = 3000;
 
@@ -14,35 +14,35 @@ let books = [
 // app.get('/index.html',(req,res)=>{
 //     res.send(index.html);
 // });
-app.use(express.static('public'));
+app.use(express.static("public"));
 // GET all books
-app.get('/books', (req, res) => {
+app.get("/books", (req, res) => {
   res.json(books);
 });
 
 // GET a single book by ID
-app.get('/books/:id', (req, res) => {
-  const book = books.find(b => b.id == req.params.id);
+app.get("/books/:id", (req, res) => {
+  const book = books.find((b) => b.id == req.params.id);
   if (!book) return res.status(404).json({ message: "Book not found" });
   res.json(book);
 });
 
 // POST a new book
-app.post('/books', (req, res) => {
+app.post("/books", (req, res) => {
   const { title, author } = req.body;
   const newBook = {
     id: books.length + 1,
     title,
-    author
+    author,
   };
   books.push(newBook);
   res.status(201).json(newBook);
 });
 
 // PUT to update a book
-app.put('/books/:id', (req, res, next) => {
+app.put("/books/:id", (req, res, next) => {
   try {
-    const book = books.find(b => b.id === req.params.id);
+    const book = books.find((b) => b.id === req.params.id);
     if (!book) {
       const error = new Error("Book not found");
       error.status = 404;
@@ -66,15 +66,35 @@ app.put('/books/:id', (req, res, next) => {
 });
 
 // DELETE a book
-app.delete('/books/:id', (req, res) => {
-  const index = books.findIndex(b => b.id == req.params.id);
+app.delete("/books/:id", (req, res) => {
+  const index = books.findIndex((b) => b.id == req.params.id);
   if (index === -1) return res.status(404).json({ message: "Book not found" });
 
   const deleted = books.splice(index, 1);
   res.json({ message: "Book deleted", book: deleted[0] });
 });
+// this is my filter part
+// i decide to choice a stardard methode filtering by the name of the author
+app.get("/books", (req, res) => {
+  const { author, genre } = req.query;
 
+  let filteredBooks = books;
+
+  if (author) {
+    filteredBooks = filteredBooks.filter(
+      (book) => book.author.toLowerCase() === author.toLowerCase()
+    );
+  }
+
+  if (genre) {
+    filteredBooks = filteredBooks.filter(
+      (book) => book.genre.toLowerCase() === genre.toLowerCase()
+    );
+  }
+
+  res.json(filteredBooks);
+});
 // Start the server
 app.listen(PORT, () => {
-  console.log(`📚 Book API running at http://localhost:${PORT}`);
+  console.log(`📚 Book API running http://localhost:${PORT}`);
 });
